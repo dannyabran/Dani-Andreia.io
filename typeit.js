2f38981b750d8d2aa20cb3f62e143a2a66ea8273
dannyabran <danielgarciaabrantes30@gmail.com>
2024-02-04 20:22:10 +0000

    data

--- typeit.js
+++ typeit.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 new TypeIt("#element", {
-    strings: ["Vamos casar!", "O papa e a mamã não fazem nada em ordem!!", "Primeiro nasci eu, então eles decidiram batizar-me!! Ah desculpem não me apresentei :)", "Eu sou o Lorenzo, o mais novo da familia", "Então como nada está em ordem, decidi pegar as redeas da vida deles", "e no dia que eles escolheram para me batizar", "eu decidi que eles se vão casar!", "Cancela tudo o que tens para fazer e junta-te a nós neste dia especial..."],
+    strings: ["Vamos casar!", "O papa e a mamã não fazem nada em ordem!!", "Primeiro nasci eu, então eles decidiram batizar-me!! Ah desculpem não me apresentei :)", "Eu sou o Lorenzo, o mais novo da familia", "Então como nada está em ordem, decidi pegar as redeas da vida deles e no dia que eles escolheram para me batizar, eu decidi que eles se vão casar!", "Cancela tudo o que tens para fazer e junta-te a nós neste dia especial...", "Marcamos encontro dia 17 de Agosto de 2024"],
     speed: 50,
     cursor: false,
     loop: false,
